154c9b7a904b0b2a04ba3b343d4e4e6207a4779e
ellyburke <117235750+ellyburke@users.noreply.github.com>
2025-01-21 12:01:25 -0500

    adding menu options

--- Lab2RapidPrototype/frontend.py
+++ Lab2RapidPrototype/frontend.py
@@ -1,21 +1,35 @@
 import csv
 
+def view():
+    pass
+
+def edit():
+    pass
+
+def add():
+    pass
+
 def printIntro():
     print("Welcome to your music catalog. Here you can view, add and edit items")
+
+def checkOptions(option):
+    if option == 1:
+        view()
+    elif option == 2:
+        add()
+    elif option == 3:
+        edit()
+    else:
+        print("Option entered is not valid. Please try again.")
+        menu()
 
 def menu():
     print("Menu:")
     print("1. View catalog")
     print("2. Add a new song")
     print("3. Edit or delete an existing song")
+    option = input("Which option would you like to select: ")
+    checkOptions(option)
+    
+menu()
 
-def readfile(file):
-    with open('music.csv', mode='r') as file:
-        f = csv.reader(file)
-        for lines in f:
-            print(lines)
-
-def writefile(file):
-    pass
-
-readfile("music.csv")
