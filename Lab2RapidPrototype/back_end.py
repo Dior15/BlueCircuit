12331e6899316e8a51913bb9e8c61f8dfc7cc3c3
Khristian Diorico <77899722+Dior15@users.noreply.github.com>
2025-02-24 13:24:04 -0500

    Delete Lab 2 Prototype

--- Lab2RapidPrototype/back_end.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-#Function to read items from csv file
-with open('Lab2RapidPrototype\music.csv', mode="r") as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        print(lines)
