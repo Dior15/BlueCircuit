864020504db3844ad6858dbe7b9d76613ba8733a
HTTime39 <132396054+HTTime39@users.noreply.github.com>
2025-03-03 16:46:02 -0500

    Added automatic rotating of the carousel

--- client/trending-carousel-temp.js
+++ client/trending-carousel-temp.js
@@ -4,6 +4,8 @@ class imageCarousel {
   // Array to hold the currently trending movies to be displayed, should hold 9
   currentDisplay = 0;
   // Indicates the array index of the left most movie currently being displayed by the image carousel
+  currentlyRotating = false;
+  // Indicates if the carousel is already rotating to stop the button from being pressed during the animation
 
   constructor() {
     let trendingMoviesString = document.getElementById("trendingCarouselLeft").getHTML();
@@ -13,18 +15,23 @@ class imageCarousel {
     document.getElementById("trendingCarouselLeft").innerHTML = "<img src=\"" + this.trendingMovies[this.currentDisplay % 9] + "\" class=\"carouselImage1\">";
     document.getElementById("trendingCarouselMain").innerHTML = "<img src=\"" + this.trendingMovies[(this.currentDisplay + 1) % 9] + "\" class=\"carouselImage2\">";
     document.getElementById("trendingCarouselRight").innerHTML = "<img src=\"" + this.trendingMovies[(this.currentDisplay + 2) % 9] + "\" class=\"carouselImage3\">";
+
   }
 
-  rotateCarousel(val) {
-  // Reassigns the index of the left most element in the carousel to be references when redrawing the carousel using the array of trending movies
-    this.currentDisplay = (this.currentDisplay + val) % 9;
-
-    if (this.currentDisplay < 0) { 
-      // Looping when negative because modulo doesn't work the way I expected it to with negative numbers(?)
-      this.currentDisplay = 8;
+  async rotateCarousel(val) {
+    if (!this.currentlyRotating) {
+      this.currentlyRotating = true;
+      // Reassigns the index of the left most element in the carousel to be references when redrawing the carousel using the array of trending movies
+        this.currentDisplay = (this.currentDisplay + val) % 9;
+    
+        if (this.currentDisplay < 0) { 
+          // Looping when negative because modulo doesn't work the way I expected it to with negative numbers(?)
+          this.currentDisplay = 8;
+        }
+    
+        this.redrawCarousel();
+        this.currentlyRotating = false;
     }
-
-    this.redrawCarousel();
   }
 
   async redrawCarousel() {
@@ -74,6 +81,16 @@ class imageCarousel {
     }, 15); 
   }
 
+  // Automatic carousel spin
+  async automaticCarouselSpin() {
+    await this.sleep(5000);
+
+    // Automatic scrolling through the image carousel
+    let interval = setInterval(function(){
+      this.rotateCarousel(1);
+    }, 5000); 
+  }
+
   // Used to pause the execution of code in the redrawCarousel method
   sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -88,3 +105,7 @@ let carouselLeftButton = document.getElementById("carouselLeftButton").addEventL
 
 let carouselRightButton = document.getElementById("carouselRightButton").addEventListener("click", function(){trendingCarousel.rotateCarousel(1)}, false);
 // Even listener for right button of image carousel
+
+let automaticInterval = setInterval(function(){
+  trendingCarousel.rotateCarousel(1);
+}, 4000);
