d12d6f58820467e990297cd02be79db27c3a104e
Khristian Diorico <77899722+Dior15@users.noreply.github.com>
2025-03-03 16:30:13 -0500

    Basic home page with Nathaniel's base carousel + search & results functionality

--- searchPage/app.py
+++ searchPage/app.py
@@ -1,32 +1,45 @@
 from flask import Flask, render_template, request
 import tmdbsimple as tmdb
 
-# Set your TMDB API key
+# Set TMDB API key
 with open("apikey.txt", "r") as file:
     tmdb.API_KEY = file.readline().strip()
-tmdb.REQUESTS_TIMEOUT = 1
-
+    
+# Initialize Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Fetch trending movies
+    popular_movies = tmdb.Movies().popular()
+    top_9_movies = popular_movies['results'][:9]  # Get top 9 trending movies
+    poster_urls = ""
+
+    # Construct full image URLs for posters
+    for movie in top_9_movies:
+        poster_url = f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"  # Movie poster
+        poster_urls += poster_url+" "
+
+    # return render_template('trending-carousel-temp.html', poster_urls=poster_urls)
+    return render_template('home.html', poster_urls=poster_urls)
 
 @app.route('/search', methods=['POST'])
 def search():
+    # Get user search input
     query = request.form.get('query')
     category = request.form.get('category')
 
     search = tmdb.Search()
 
+    # Perform search based on category
     if category == "movie":
-        response = search.movie(query=query)
+        search.movie(query=query)
     elif category == "tv":
-        response = search.tv(query=query)
+        search.tv(query=query)
     elif category == "person":
-        response = search.person(query=query)
+        search.person(query=query)
     else:
-        return "Invalid category selected", 400
+        return "Invalid category", 400  # Error handling
 
     return render_template('results.html', results=search.results, category=category, query=query)
 
