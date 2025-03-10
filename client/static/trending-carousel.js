abf522a9795132786d76e7745f117537e9a5a596
HTTime39 <132396054+HTTime39@users.noreply.github.com>
2025-03-10 11:52:20 -0400

    Visual changes to carousel and code trimming

--- client/static/trending-carousel.js
+++ client/static/trending-carousel.js
@@ -12,8 +12,9 @@ class imageCarousel {
     let trendingMoviesString = document.getElementById("trend1").getHTML();
     this.trendingMovies = trendingMoviesString.split(' ');
     this.trendingCarousel = document.getElementById("trendingCarousel");
-    // Retrieving 
+    // Retrieving links to images of the most popular movies
 
+    // Assigning each movie into a div within the image carousel
     document.getElementById("trend1").innerHTML = "<img src=\"" + this.trendingMovies[0] + "\" class=\"carouselImage1\">";
     document.getElementById("trend2").innerHTML = "<img src=\"" + this.trendingMovies[1] + "\" class=\"carouselImage2\">";
     document.getElementById("trend3").innerHTML = "<img src=\"" + this.trendingMovies[2] + "\" class=\"carouselImage3\">";
@@ -27,6 +28,7 @@ class imageCarousel {
   }
 
   async rotateCarousel(direction) {
+    // Rotates the carousel based on the input direction, the direction is a value -1 or 1 specified by which button/method calls the rotateCarousel method
     if (!this.currentlyRotating && document.hasFocus()) { // document.hasFocus() stops weird flickering when tabbing out and back in
       this.currentlyRotating = true; // Prevents from this function running again while the animation is still running
       this.currentDisplay += direction;
@@ -42,24 +44,13 @@ class imageCarousel {
       }
       
 
-      await this.sleep(300);
+      await this.sleep(350); // Waits 350ms before setting currently rotating to false to give time for the animation to finish before allowing the carousel to begin rotating again
 
       this.currentlyRotating = false;  
     }
   }
 
-  // Automatic carousel spin
-  automaticSpinDelay = 5000;
-  async automaticCarouselSpin() {
-    await this.sleep(automaticSpinDelay);
-
-    // Automatic scrolling through the image carousel
-    let interval = setInterval(function(){
-      this.rotateCarousel(1);
-    }, automaticSpinDelay); 
-  }
-
-  // Used to pause the execution of code in the redrawCarousel method
+  // Used to pause the execution of code in the rotateCarousel method
   sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
@@ -72,9 +63,7 @@ let carouselLeftButton = document.getElementById("carouselLeftButton").addEventL
 // Event listener for left button of image carousel
 
 let carouselRightButton = document.getElementById("carouselRightButton").addEventListener("click", function(){trendingCarousel.rotateCarousel(1)}, false);
-// Even listener for right button of image carousel
+// Event listener for right button of image carousel
 
-let automaticInterval = setInterval(function(){
-  trendingCarousel.rotateCarousel(1);
-}, 4000);
-
+let automaticInterval = setInterval(function(){trendingCarousel.rotateCarousel(1);}, 4000);
+//Automatically spinning the carousel every 4 seconds
