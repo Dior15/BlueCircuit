438e89ec0d5a475a088ae634efb692c98278e0d2
Khristian Diorico <77899722+Dior15@users.noreply.github.com>
2025-03-31 13:57:09 -0400

    Finished watchlist functionality

--- client/MovieFlix.bat
+++ client/MovieFlix.bat
@@ -1,7 +1,7 @@
 @echo off
 @REM Change this to your BlueCircuit directory
-cd /d "C:\Users\Elllycea\OntarioTech\csci2040u\BlueCircuit\client"
-start cmd /k "C:\Users\Elllycea\AppData\Local\Programs\Python\Python310\python.exe app.py"
+cd /d ""
+start cmd /k "client/app.py"
 timeout /t 2 >nul
 start "" "http://127.0.0.1:5000"
 exit
