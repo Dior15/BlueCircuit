b0676c43642aadab18aa0b0a2f9bc734259ecff2
ellyburke <117235750+ellyburke@users.noreply.github.com>
2025-03-31 13:50:23 -0400

    adding styling

--- client/static/addToWatchlist.js
+++ client/static/addToWatchlist.js
@@ -1,4 +1,5 @@
 document.getElementById('add-to-watchlist').onclick = function() {
     // singnify on the front end that the user has added the movie to the watchlist
-    document.getElementById('add-to-watchlist').innerHTML = "Added!";
+    document.getElementById('add-to-watchlist').innerHTML = "&#10004; Added!"
+    document.getElementById('add-to-watchlist').style.backgroundColor = "#1c1c1c"
 }
